0f9d80c73c09c8ee5081a7503d7df3f347a68e3c
Joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-12-04 16:46:51 -0300

    finished array cardio

--- 07 - Array Cardio Day 2/js.js
+++ 07 - Array Cardio Day 2/js.js
@@ -1,5 +1,5 @@
 // ## Array Cardio Day 2
-
+const currentYear = new Date().getFullYear();
 const people = [
   {
     name: 'Wes',
@@ -44,12 +44,20 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+console.log(people.some(person => currentYear - person.year >= 19));
 // Array.prototype.every() // is everyone 19 or older?
-
+console.log(people.every(person => currentYear - person.year >= 19));
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
-
+console.log(comments.find(comment => comment.id === 823423).text);
 // Array.prototype.findIndex()
 // Find the comment with this ID
+const findId = comments.findIndex(comment => comment.id === 823423);
+// comments.splice(index, 1);
+const newComments = [
+  ...comments.slice(0, findId),
+  ...comments.slice(findId + 1),
+];
+console.table(newComments);
 // delete the comment with the ID of 823423
